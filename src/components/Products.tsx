d4a1d619eb27f430a708262083f1a271dd002654
Edward Tanguay (windows) <edward@tanguay.info>
2025-09-29 21:56:14 +0200

    Load both types

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
-import type { Category } from "../types";
+import { type Product, type Category } from "../types";
+import axios from "axios";
 
 const baseApiUrl = 'http://localhost:3399';
 const token = 'abcde12345';
 
 export const Products = () => {
 	const [categories, setCategories] = useState<Category[]>([]);
+	const [products, setProducts] = useState<Product[]>([]);
 
 	useEffect(() => {
 		(async () => {
@@ -17,12 +19,25 @@ export const Products = () => {
 			const _categories = await response.json();
 			setCategories(_categories);
 		})();
+	});
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(`${baseApiUrl}/northwind/products`, {
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			});
+			const _products = response.data;
+			setProducts(_products);
+		})();
 	})
 
 	return (
 		<div className="area areaProduct">
 			<h2>Products</h2>
 			<p>There are {categories.length} categories.</p>
+			<p>There are {products.length} products.</p>
 		</div>
 	)
 }
