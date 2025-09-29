d7237eb60282690b84f308ded6a9d02127a4f312
Edward Tanguay (windows) <edward@tanguay.info>
2025-09-29 21:47:03 +0200

    getting Products

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
+import type { Category } from "../types";
+
+const baseApiUrl = 'http://localhost:3399';
+const token = 'abcde12345';
+
 export const Products = () => {
+	const [categories, setCategories] = useState<Category[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await fetch(`${baseApiUrl}/northwind/categories`, {
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			});
+			const _categories = await response.json();
+			setCategories(_categories);
+		})();
+	})
+
 	return (
 		<div className="area areaProduct">
 			<h2>Products</h2>
-			<p>These are the products.</p>
+			<p>There are {categories.length} categories.</p>
 		</div>
 	)
 }
