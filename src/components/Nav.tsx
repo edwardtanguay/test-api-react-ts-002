3ba1734c6fb354fb73d036daf9e4c74f50a9e4e2
Edward Tanguay (windows) <edward@tanguay.info>
2025-09-30 10:26:14 +0200

    FEATURE: Test003 search box

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,6 +6,7 @@ export const Nav = () => {
 			<div><NavLink to="/">HOME</NavLink></div>
 			<div><NavLink to="/test001">Test001</NavLink></div>
 			<div><NavLink to="/test002">Test002</NavLink></div>
+			<div><NavLink to="/test003">Test003</NavLink></div>
 		</nav>
 	)
 }
---
+++ src/components/Test003/Test003.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useState, type ChangeEvent } from "react";
+import type { DisplayProduct, Category, Product } from "../../types";
+import "./styles.css";
+
+const baseApiUrl = 'http://localhost:3399';
+const token = 'abcde12345';
+
+
+export const Test003 = () => {
+	const [products, setProducts] = useState<Product[]>([]);
+	const [categories, setCategories] = useState<Category[]>([]);
+	const [searchText, setSearchText] = useState("")
+	const [displayProducts, setDisplayProducts] = useState<DisplayProduct[]>([]);
+	const [filteredDisplayProducts, setfilteredDisplayProducts] = useState<DisplayProduct[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await fetch(`${baseApiUrl}/northwind/products`, {
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			});
+			const _products = await response.json();
+			setProducts(_products);
+		})();
+	}, [])
+
+	useEffect(() => {
+		(async () => {
+			const response = await fetch(`${baseApiUrl}/northwind/categories`, {
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			});
+			const _categories = await response.json();
+			setCategories(_categories);
+		})();
+	}, [])
+
+	useEffect(() => {
+		if (categories.length > 0 && products.length > 0) {
+			const _displayProducts: DisplayProduct[] = products.map(p => {
+				const category = categories.find(c => c.categoryID === p.categoryID)
+				return {
+					productName: p.name,
+					productCategory: category ? category.name : "(UNKNOWN)"
+				}
+			})
+			setDisplayProducts(_displayProducts);
+		}
+
+	}, [categories, products])
+
+	const handleSearchBoxTyping = (e:ChangeEvent<HTMLInputElement>) => {
+		const _searchText = e.target.value;
+		setSearchText(_searchText);
+		const _filteredDisplayProducts = displayProducts.filter(dp => dp.productCategory.toLowerCase().includes(_searchText.toLowerCase()) || dp.productName.toLowerCase().includes(_searchText.toLowerCase()));
+		setfilteredDisplayProducts(_filteredDisplayProducts);
+	}
+
+	return (
+		<div className="area areaTest003">
+			<h2>Test003: Search box</h2>
+			<div className="searchArea">
+				<input value={searchText} onChange={(e) => handleSearchBoxTyping(e)} />
+			</div>
+			<ul>
+				{filteredDisplayProducts.map((dp, idx) => {
+					return (
+						<li key={idx}>{dp.productName} ({dp.productCategory})</li>
+					)
+				})}
+			</ul>
+		</div>
+	)
+}
