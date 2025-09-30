922fbbb3928e0d1025a69e767d76637546c15f08
Edward Tanguay (windows) <edward@tanguay.info>
2025-09-30 08:53:53 +0200

    Create product state var

--- src/components/Test002.tsx
+++ src/components/Test002.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+import type { Product } from "../types";
+
 export const Test002 = () => {
+	const [products, setProducts] = useState<Product[]>([]);
+
 	return (
 		<div className="area areaTest002">
 			<h2>Test002: Choose Category to See Products</h2>
-			<p>todo</p>
+			<p>There are {products.length} products.</p>
 		</div>
 	)
 }
