03652038fc941b3f1dd3044a1a99fdea5c50f090
Edward Tanguay (windows) <edward@tanguay.info>
2025-09-30 09:04:20 +0200

    Switch fetch to axios

--- src/components/Test002.tsx
+++ src/components/Test002.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import type { Category, Product } from "../types";
+import axios from "axios";
 
 const baseApiUrl = 'http://localhost:3399';
 const token = 'abcde12345';
@@ -10,12 +11,12 @@ export const Test002 = () => {
 
 	useEffect(() => {
 		(async () => {
-			const response = await fetch(`${baseApiUrl}/northwind/products`, {
+			const response = await axios.get(`${baseApiUrl}/northwind/products`, {
 				headers: {
 					Authorization: `Bearer ${token}`
 				}
 			});
-			const _products = await response.json();
+			const _products = response.data;
 			setProducts(_products);
 		})();
 	}, [])
