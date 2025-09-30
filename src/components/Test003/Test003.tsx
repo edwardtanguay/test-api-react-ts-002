fdb570faed98ba7063f605a0dc4ee6c619360d9b
Edward Tanguay (windows) <edward@tanguay.info>
2025-09-30 09:55:51 +0200

    Fetch products

--- src/components/Test003/Test003.tsx
+++ src/components/Test003/Test003.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import type { Product } from "../../types";
+
+const baseApiUrl = 'http://localhost:3399';
+const token = 'abcde12345';
+
+
 export const Test003 = () => {
+	const [products, setProducts] = useState<Product[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await fetch(`${baseApiUrl}/northwind/products`, {
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			});
+			const _products = await response.json();
+			setProducts(_products);
+		})();
+	}, [])
+
 	return (
 		<div className="area areaTest003">
 			<h2>Test003: Search box</h2>
+			<p>There are {products.length} products.</p>
 		</div>
 	)
 }
